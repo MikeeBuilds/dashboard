983f21d17ae735a1b7c934add6009eaec6f778ef
Algo Hussle <citizensofandrometta@gmail.com>
2023-06-27 19:19:18 -0500

    first commit

---
+++ wallet-dapp/pages/_app.js
@@ -0,0 +1,65 @@
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import {
+	mainnet,
+	polygon,
+	optimism,
+	arbitrum,
+	goerli,
+	polygonMumbai,
+	optimismGoerli,
+	arbitrumGoerli,
+} from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import MainLayout from "../layout/mainLayout";
+
+const { chains, provider } = configureChains(
+	[
+		mainnet,
+		goerli,
+		polygon,
+		polygonMumbai,
+		optimism,
+		optimismGoerli,
+		arbitrum,
+		arbitrumGoerli,
+	],
+	[publicProvider({ apiKey: process.env.ALCHEMY_API_KEY }), publicProvider()]
+);
+
+console.log(process.env.WALLETCONNECT_PROJECT_ID);
+
+const { connectors } = getDefaultWallets({
+	appName: "Algo Hussle Wallet Dapp",
+	chains,
+	projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+});
+
+const wagmiClient = createClient({
+	autoConnect: true,
+	connectors,
+	provider,
+});
+
+export { WagmiConfig, RainbowKitProvider };
+function MyApp({ Component, pageProps }) {
+	return (
+		<WagmiConfig client={wagmiClient}>
+			<RainbowKitProvider
+				modalSize="compact"
+				initialChain={process.env.NEXT_PUBLIC_DEFAULT_CHAIN}
+				chains={chains}
+			>
+				<MainLayout>
+					<Component {...pageProps} />
+				</MainLayout>
+			</RainbowKitProvider>
+		</WagmiConfig>
+	);
+}
+
+export default MyApp;
