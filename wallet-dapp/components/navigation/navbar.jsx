2f59d3d16cbd92c5ca1c72b607adf64300674098
Algo Hussle <citizensofandrometta@gmail.com>
2023-06-27 19:32:14 -0500

    css changes

--- wallet-dapp/components/navigation/navbar.jsx
+++ wallet-dapp/components/navigation/navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
 			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
 				<img
 					className={styles.alchemy_logo}
-					src="/alchemy_logo.svg"
+					src="/logo.svg"
 				></img>
 			</a>
 			<ConnectButton></ConnectButton>
